17a36ea46d39ed73be64ec0c322512d65f090f22
Ashik Raj <ashikraj.78@gmail.com>
2023-06-06 11:52:42 +0530

    adding node click feature

--- src/components/BinaryTreeDataVisual/index.js
+++ src/components/BinaryTreeDataVisual/index.js
@@ -9,54 +9,75 @@ function BinaryTreeDataVisual({ data }) {
     const svg = d3.select("svg").attr("width", width).attr("height", height);
 
     const padding = 50;
-    const treeLayout = d3.tree().size([width, height - 2 * padding]);
+    const treeLayout = d3
+      .tree()
+      .size([width - 2 * padding, height - 2 * padding]);
 
-    const root = d3.hierarchy(data, (d) => d.children);
-    const links = treeLayout(root).links();
-    const nodes = root.descendants();
+    const dataCopy = JSON.parse(JSON.stringify(data));
+    const root = d3.hierarchy(dataCopy, (d) => d.children);
+    treeLayout(root);
+
+    const clearHighlights = (node) => {
+      if (!node) return;
+      node.data.highlight = false;
+      if (node.children) {
+        node.children.forEach(clearHighlights);
+      }
+    };
 
     const highlightPath = (node) => {
       if (!node) return;
-      // console.log("thisis cick");
-
-      node.highlight = true;
-      highlightPath(node.parent);
+      node.data.highlight = true;
+      if (node.parent) highlightPath(node.parent);
     };
 
-    const g = svg
-      .append("g")
-      .attr("transform", `translate(${padding},${padding})`);
+    const update = (root) => {
+      // eslint-disable-next-line no-unused-vars
+      const link = svg
+        .selectAll(".link")
+        .data(root.links(), (d) => d.target.id)
+        .join("path")
+        .attr(
+          "class",
+          (d) => `link ${d.target.data.highlight ? "highlight" : ""}`
+        )
+        .attr(
+          "d",
+          (d) =>
+            `M ${d.source.x + padding},${d.source.y + padding} L ${
+              d.target.x + padding
+            },${d.target.y + padding}`
+        );
 
-    g.selectAll(".link")
-      .data(links)
-      .enter()
-      .append("path")
-      .attr("class", "link")
-      .attr("d", (d) => {
-        return `
-          M${d.source.x},${d.source.y}
-          L${d.target.x},${d.target.y}
-        `;
-      });
+      const g = svg
+        .selectAll("g")
+        .data(root.descendants(), (d) => d.id || (d.id = Math.random()))
+        .join("g")
+        .attr(
+          "transform",
+          (d) => `translate(${d.x + padding},${d.y + padding})`
+        )
+        .attr("class", (d) => `node ${d.data.highlight ? "highlight" : ""}`)
+        .on("click", (event, d) => {
+          clearHighlights(root);
+          highlightPath(d);
+          update(root);
+        });
 
-    g.selectAll(".node")
-      .data(nodes)
-      .enter()
-      .append("g")
-      .attr("class", "node")
-      .attr("transform", (d) => `translate(${d.x},${d.y})`)
-      .on("click", (event, d) => {
-        highlightPath(d);
-        // update(root);
-      });
+      g.selectAll("circle")
+        .data((d) => [d])
+        .join("circle")
+        .attr("r", 20);
 
-    g.selectAll(".node").append("circle").attr("r", 20);
+      g.selectAll("text")
+        .data((d) => [d])
+        .join("text")
+        .attr("dx", -10)
+        .attr("dy", 5)
+        .text((d) => d.data.name);
+    };
 
-    g.selectAll(".node")
-      .append("text")
-      .attr("dx", -10)
-      .attr("dy", 5)
-      .text((d) => d.data.name);
+    update(root);
   }, [data]);
 
   return (
